d4d5eb7932b429f571eec949c0b792d32e932bf2
Betty0721 <bettyyimer07@gmail.com>
2022-04-09 01:07:38 +0300

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- using puts
+ * Return: returns value
+ * Description: about the file
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
