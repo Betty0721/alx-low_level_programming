f533abf50a39e7d7e0430beb90a580cf5caccdaf
Betty0721 <bettyyimero721>
2022-04-12 23:55:01 +0300

    100x

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,116 @@
 #include "holberton.h"
 
 /**
- * print_times_table - Print the `n` times table, starting with 0.
- * Description: If `n` is greater than 15 or less than 0, print nothing.
- * @n: int type number
+ * print_times_table - prints the n times table, starting with 0.
+ *
+ * Return : void
  */
 void print_times_table(int n)
 {
-	int x = 0, y, z;
+	int i, j, res;
 
-	if (n > 15 || n < 0)
-		return;
-	while (x <= n)
+	if ((n > 15) || (n < 0))
 	{
-		for (y = 0; y <= n; y++)
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
 		{
-			z = x * y;
-			if (z > 99)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(z / 100 + '0');
-				_putchar((z / 10 % 10) + '0');
-				_putchar(z % 10 + '0');
+				res = i * j;
+				if (res < 10)
+				{
+					if (j == 0)
+						pr_3_digit(res, 0);
+					else if (j == n)
+						pr_3_digit(res, 1);
+					else
+						pr_3_digit(res, 2);
+				}
+				else if ((res >= 10) && (res < 100))
+				{
+					if (j == n)
+						pr_3_digit(res, 3);
+					else
+						pr_3_digit(res, 4);
+				}
+				else
+				{
+					if (j == n)
+						pr_3_digit(res, 5);
+					else
+						pr_3_digit(res, 6);
+				}
 			}
-			else if (z > 9)
-			{
-				_putchar(' ');
-				_putchar(z / 10 + '0');
-				_putchar(z % 10 + '0');
-			}
-			else if (y != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-			}
-			else
-				_putchar(z + '0');
-
-			if (y != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
 		_putchar('\n');
-		x++;
+		}
+	}
+}
+
+/**
+ * print_spaces - print given spaces using _putchar.
+ * @n: Number of spaces
+ *
+ * Return : void
+*/
+void print_spaces(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(' ');
+	}
+}
+
+/**
+ * pr_3_digit - prints the n times table, starting with 0.
+ * @charcode: Character code
+ * @fmt: Format
+ *
+ * Return : void
+ */
+void pr_3_digit(int charcode, int fmt)
+{
+	switch (fmt)
+	{
+		case 0:
+			_putchar(charcode + '0');
+			_putchar(',');
+			break;
+		case 1:
+			print_spaces(3);
+			_putchar(charcode + '0');
+			break;
+		case 2:
+			print_spaces(3);
+			_putchar(charcode + '0');
+			_putchar(',');
+			break;
+		case 3:
+			print_spaces(2);
+			_putchar((charcode / 10) + '0');
+			_putchar((charcode % 10) + '0');
+			break;
+		case 4:
+			print_spaces(2);
+			_putchar((charcode / 10) + '0');
+			_putchar((charcode % 10) + '0');
+			_putchar(',');
+			break;
+		case 5:
+			print_spaces(1);
+			_putchar((charcode / 100) + '0');
+			_putchar((charcode / 10) % 10 + '0');
+			_putchar((charcode % 10) + '0');
+			break;
+		case 6:
+			print_spaces(1);
+			_putchar((charcode / 100) + '0');
+			_putchar((charcode / 10) % 10 + '0');
+			_putchar((charcode % 10) + '0');
+			_putchar(',');
+			break;
 	}
 }
